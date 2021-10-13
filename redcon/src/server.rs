903524bf88db2f9c065ce6da8336cafcc3c51f0c
szuwgh <895340293@qq.com>
2021-10-13 22:34:42 +0800

    add future

--- redcon/src/server.rs
+++ redcon/src/server.rs
@@ -1,5 +1,8 @@
+use std::future::Future;
 use tokio::net::{TcpListener, TcpStream};
 
 type handler_cmd = fn();
 
-pub async fn run(listener: TcpListener, handler: handler_cmd) {}
+pub async fn run(listener: TcpListener, shutdown: impl Future, handler: handler_cmd) {
+    loop {}
+}
