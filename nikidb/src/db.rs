56641cda9a035a56a555bed16b40111598219c41
szuwgh <895340293@qq.com>
2021-11-03 17:03:08 +0800

    fix some bug

--- nikidb/src/db.rs
+++ nikidb/src/db.rs
@@ -13,9 +13,7 @@ use std::io::ErrorKind;
 use std::mem;
 use std::sync::{Arc, Mutex, RwLock};
 
-trait LevelGet {}
-
-trait LevelPut {}
+struct IndexEntry {}
 
 #[derive(Clone)]
 pub struct DbDropGuard {
@@ -32,28 +30,6 @@ impl DbDropGuard {
         self.db.clone()
     }
 }
-
-// #[derive(Clone)]
-// pub struct DBHandler {
-//     pub db: DB,
-// }
-
-// impl DBHandler {
-//     pub fn new(options: Options) -> DBHandler {
-//         let db = DB::open(options).unwrap();
-//         DBHandler { db }
-//     }
-//     pub fn put(&self, key: &[u8], value: &[u8]) {
-//         // let mut db = self.db.lock().unwrap();
-//         //db.put(key, value)
-//         //  drop(db);
-//     }
-//     pub fn get(&self, key: &[u8]) {
-//         // let mut db = self.db.lock().unwrap();
-//         // db.read(key)
-//         // drop(db);
-//     }
-// }
 
 #[derive(Clone)]
 pub struct DB {
@@ -102,6 +78,8 @@ struct ActiveUnit {
     file_size: usize,
     //data dir path
     data_dir: String,
+    //froze archived files ,can't motify data
+    froze_archived_files: Option<ArchivedUnit>,
 }
 
 impl ActiveUnit {
@@ -113,6 +91,7 @@ impl ActiveUnit {
             indexes: HashMap::new(),
             file_size: file_size as usize,
             data_dir: data_dir,
+            froze_archived_files: None,
         };
         active_unit.load_index();
         Ok(active_unit)
@@ -190,6 +169,9 @@ impl ActiveUnit {
 
         self.archived_files
             .insert(active_file_id, old_active_data_file);
+        if self.archived_files.len() > 10 {
+            self.froze_archived_files = Some(self.to_archived_unit());
+        }
         Ok(offset)
     }
 
@@ -199,6 +181,18 @@ impl ActiveUnit {
             .get(&key.to_vec())
             .ok_or(Error::from(ErrorKind::Interrupted))?;
         self.active_file.get(*offset)
+    }
+
+    fn to_archived_unit(&mut self) -> ArchivedUnit {
+        let new_archived_files: HashMap<u32, DataFile> = HashMap::new();
+        let new_indexes: HashMap<Vec<u8>, u64> = HashMap::new();
+        let old_archived_files = mem::replace(&mut self.archived_files, new_archived_files);
+        let old_indexes = mem::replace(&mut self.indexes, new_indexes);
+        let archived_unit = ArchivedUnit {
+            archived_files: old_archived_files,
+            indexes: old_indexes,
+        };
+        archived_unit
     }
 }
 
@@ -246,20 +240,9 @@ fn build_data_file(dir_path: &str, size: u64) -> IoResult<(DataFile, HashMap<u32
         return Ok((active_file, files_map));
     }
     let mut files: Vec<u32> = Vec::new();
-    // let mut files_id_map: HashMap<DataType, Vec<u32>> = HashMap::new();
-    // let mut active_files: HashMap<DataType, DataFile> = HashMap::new();
     for n in names.iter() {
         let split_name: Vec<&str> = n.split(".").collect();
-        // let data_type = match split_name[2] {
-        //     DATA_TYPE_ZSET => DataType::ZSet,
-        //     DATA_TYPE_STR => DataType::String,
-        //     DATA_TYPE_HASH => DataType::Hash,
-        //     DATA_TYPE_LIST => DataType::List,
-        //     DATA_TYPE_SET => DataType::Set,
-        //     _ => continue,
-        // };
         let id = result_skip_fail!(split_name[0].parse::<u32>());
-        // let id_vec = files_id_map.entry(data_type).or_insert(Vec::new());
         files.push(id);
     }
 
@@ -271,20 +254,16 @@ fn build_data_file(dir_path: &str, size: u64) -> IoResult<(DataFile, HashMap<u32
         );
     }
 
-    //  active_files.insert(k, DataFile::new(dir_path, 0, k)?);
-
     Ok((active_file, files_map))
 }
 
 impl DB {
     pub fn open(options: Options) -> IoResult<DB> {
-        //create database dir
         let dir_path = options
             .data_dir
             .to_str()
             .ok_or(Error::from(ErrorKind::Interrupted))?;
         fs::create_dir_all(dir_path).map_err(|err| Error::new(ErrorKind::Interrupted, err))?;
-        //let (active_files, archived_files) = build_data_file(dir_path)?;
         let data_dir = options
             .data_dir
             .to_str()
@@ -294,12 +273,9 @@ impl DB {
                 options.file_size,
                 data_dir.to_owned(),
             )?)),
-            // active_file_map: active_files,
-            // archived_files: archived_files,
-            // indexes: HashMap::new(),
+
             options: options,
         };
-        //    db.load_index();
         Ok(db)
     }
 
@@ -312,69 +288,6 @@ impl DB {
         let levels = self.levels.read().unwrap();
         levels.get(key)
     }
-
-    // fn load_index(&mut self) {
-    //     for (_, files_map) in self.archived_files.iter_mut() {
-    //         let mut id_vec = files_map
-    //             .iter_mut()
-    //             .map(|(_id, _)| _id.clone())
-    //             .collect::<Vec<u32>>();
-    //         id_vec.sort();
-    //         for id in id_vec.iter() {
-    //             let data_file = files_map.get_mut(id).unwrap();
-    //             let mut iter = data_file.iterator();
-    //             let mut offset: u64 = 0;
-    //             loop {
-    //                 let e = iter.next();
-    //                 let entry = match e {
-    //                     Ok(entry) => entry,
-    //                     Err(_) => break,
-    //                 };
-    //                 self.indexes.insert(entry.key.clone(), offset);
-    //                 offset += entry.size() as u64;
-    //             }
-    //         }
-    //     }
-    //     for (_, file) in self.active_file_map.iter_mut() {
-    //         let mut iter = file.iterator();
-    //         let mut offset: u64 = 0;
-    //         loop {
-    //             let e = iter.next();
-    //             let entry = match e {
-    //                 Ok(entry) => entry,
-    //                 Err(_) => break,
-    //             };
-    //             self.indexes.insert(entry.key.clone(), offset);
-    //             offset += entry.size() as u64;
-    //         }
-    //         file.offset = offset;
-    //     }
-    // }
-
-    // pub fn put(&mut self, key: &[u8], value: &[u8]) -> IoResult<u64> {
-    //     let e = Entry {
-    //         timestamp: time::get_time_unix_nano() as u64,
-    //         key: key.to_vec(),
-    //         value: value.to_vec(),
-    //         crc: 0,
-    //     };
-    //     let offset = self.store(&e)?;
-    //     self.indexes.insert(e.key, offset);
-    //     Ok(offset)
-    // }
-    // //
-
-    // pub fn read(&mut self, key: &[u8]) -> IoResult<Entry> {
-    //     let offset = self
-    //         .indexes
-    //         .get(&key.to_vec())
-    //         .ok_or(Error::from(ErrorKind::Interrupted))?;
-    //     let active_data_file = self
-    //         .active_file_map
-    //         .get_mut(&DataType::String)
-    //         .ok_or(Error::from(ErrorKind::Interrupted))?;
-    //     active_data_file.read(*offset)
-    // }
 
     pub fn merge() {}
 }
@@ -398,17 +311,4 @@ mod tests {
         let value = d.get("a".as_bytes()).unwrap().value;
         println!("{:?}", String::from_utf8(value).unwrap());
     }
-    // fn test_build_data_file() {
-    //     let (active_file_map, archived_files) = build_data_file("./db").unwrap();
-    //     for (k, v) in active_file_map {
-    //         println!("{:?},{:?}", k, v.file_id);
-    //     }
-
-    //     for (k, v) in archived_files {
-    //         println!("{:?}", k);
-    //         for (k1, v1) in v {
-    //             println!("{:?},{:?}", k1, v1.file_id);
-    //         }
-    //     }
-    // }
 }
