122a125140f3da28b6882489ce52c93e90776e5a
szuwgh <895340293@qq.com>
2022-03-30 18:59:30 +0800

    save

--- nikikv/src/db.rs
+++ nikikv/src/db.rs
@@ -1,13 +1,28 @@
+use crate::error::NKError;
+use crate::error::NKResult;
+use std::fs::OpenOptions;
+
 pub struct DB {
     //  file: File,
 }
 
-pub struct MyStruct {}
+#[derive(Debug)]
+pub struct MyStruct {
+    a: u32,
+    b: u64,
+}
 
 impl DB {
-    pub fn open(path: &str) {
-        // let f = OpenOptions::new
+    pub fn open(db_path: &str) -> NKResult<DB> {
+        let f = OpenOptions::new()
+            .read(true)
+            .write(true)
+            .open(db_path)
+            .map_err(|e| NKError::OpenDBFail(e))?;
+        Ok(DB {})
     }
+
+    fn init(&mut self) {}
 }
 
 pub fn u8_to_value(v: &[u8]) -> MyStruct {
@@ -15,13 +30,19 @@ pub fn u8_to_value(v: &[u8]) -> MyStruct {
     s
 }
 
-unsafe fn any_as_u8_slice<T: Sized>(p: &T) -> &[u8] {
-    std::slice::from_raw_parts((p as *const T) as *const u8, ::std::mem::size_of::<T>())
+fn any_as_u8_slice<T: Sized>(p: &T) -> &[u8] {
+    unsafe { std::slice::from_raw_parts((p as *const T) as *const u8, ::std::mem::size_of::<T>()) }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
     #[test]
-    fn test_struct_to_slice() {}
+    fn test_struct_to_slice() {
+        let s: MyStruct = MyStruct { a: 123, b: 456 };
+        let b = any_as_u8_slice(&s);
+        println!("b:{:?}", b);
+        let a: MyStruct = u8_to_value(b);
+        println!("MyStruct:{:?}", a);
+    }
 }
--- nikikv/src/error.rs
+++ nikikv/src/error.rs
@@ -1,9 +1,10 @@
 use std::io;
 use thiserror::Error;
-pub type NKResult<T> = Result<T, Error>;
+
+pub type NKResult<T> = Result<T, NKError>;
 
 #[derive(Error, Debug)]
-pub enum Error {
+pub enum NKError {
     #[error("open db fail :{0}")]
     OpenDBFail(io::Error),
 }
